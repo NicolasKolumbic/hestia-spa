b1a502ce2fcfa4e2a3afb4792c376da9a7a8fd7f
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2025-12-29 19:08:32 -0300

    feat: add new Energy component with its template, styles, and tests

---
+++ src/app/features/private/devices/energy/energy.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-energy',
+  imports: [],
+  templateUrl: './energy.html',
+  styleUrl: './energy.css',
+})
+export class Energy {
+
+}
