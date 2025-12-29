87882ad41af8e4e21b920402b7912c22636ac6dc
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2025-12-29 19:10:05 -0300

    feat: add initial automation component

---
+++ src/app/features/private/automation/automation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-automation',
+  imports: [],
+  templateUrl: './automation.html',
+  styleUrl: './automation.css',
+})
+export class Automation {
+
+}
