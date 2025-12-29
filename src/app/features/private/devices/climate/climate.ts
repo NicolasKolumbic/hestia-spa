79ef89f773264714d444189f6bbb37c54338967c
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2025-12-29 19:07:37 -0300

    feat: implement climate device component

---
+++ src/app/features/private/devices/climate/climate.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-climate',
+  imports: [],
+  templateUrl: './climate.html',
+  styleUrl: './climate.css',
+})
+export class Climate {
+
+}
