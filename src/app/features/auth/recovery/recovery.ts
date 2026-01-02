778de10108dd31da8aa01979b4f68c2523695dc2
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-01-02 14:29:13 -0300

    feat: Implement password recovery page with UI and component logic.

--- src/app/features/auth/recovery/recovery.ts
+++ src/app/features/auth/recovery/recovery.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recovery',
-  imports: [],
+  imports: [
+    ButtonModule,
+    InputTextModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './recovery.html',
   styleUrl: './recovery.css',
 })
