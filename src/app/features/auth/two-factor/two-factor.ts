c09f7c64f95b1a8403e7f599ec839e576109f1e9
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-01-02 14:29:41 -0300

    feat: implement two-factor authentication component with its TypeScript logic and HTML template.

--- src/app/features/auth/two-factor/two-factor.ts
+++ src/app/features/auth/two-factor/two-factor.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputOtpModule } from 'primeng/inputotp';
+import { InputMaskModule } from 'primeng/inputmask';
+import { FormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-two-factor',
-  imports: [],
+  imports: [
+    ButtonModule,
+    InputTextModule,
+    CommonModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    InputOtpModule,
+    InputMaskModule,
+    FormsModule
+  ],
   templateUrl: './two-factor.html',
   styleUrl: './two-factor.css',
 })
 export class TwoFactor {
-
+  otpValue: string = '';
 }
