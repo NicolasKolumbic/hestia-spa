24aa521765c33ff88bb257aa2d738ff3a3169975
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-01-02 14:28:54 -0300

    feat: Implement initial login page UI and component logic.

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { HestiaBrand } from "@shared/components/hestia-brand/hestia-brand";
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    CheckboxModule,
+    RouterLink,
+    HestiaBrand
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private readonly _formBuilder = inject(FormBuilder);
 
+  protected loginForm: FormGroup = this._formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    remember: [false]
+  });
+
+  protected onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted', this.loginForm.value);
+      // TODO: Implement Auth Service integration
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
