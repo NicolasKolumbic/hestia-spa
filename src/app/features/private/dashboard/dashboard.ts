ad450cb5e7021329cfcd96abbe1bcc5e2215c1fb
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-01-09 22:15:09 -0300

    feat: dashboard section page

--- src/app/features/private/dashboard/dashboard.ts
+++ src/app/features/private/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
