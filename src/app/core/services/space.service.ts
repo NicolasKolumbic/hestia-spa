ae245613de998f20704c65c34acda1289aeca688
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-02-09 22:17:08 -0300

    feat: Add Dashboard component and SpaceService for site CRUD operations.

--- src/app/core/services/space.service.ts
+++ src/app/core/services/space.service.ts
@@ -15,4 +15,26 @@ export class SpaceService {
       map((sites: SiteDto[]) => sites.map(site => new Site(site.siteId, site.name, site.icon, site.address)))
     );
   }
+
+  findById(siteId: string): Observable<Site> {
+    return this.http.get<Site>(`http://localhost:3000/api/sites/${siteId}`).pipe(
+      map((site: SiteDto) => new Site(site.siteId, site.name, site.icon, site.address))
+    );
+  }
+
+  create(site: SiteDto): Observable<Site> {
+    return this.http.post<Site>('http://localhost:3000/api/sites', site).pipe(
+      map((site: SiteDto) => new Site(site.siteId, site.name, site.icon, site.address))
+    );
+  }
+
+  update(site: SiteDto): Observable<Site> {
+    return this.http.put<Site>(`http://localhost:3000/api/sites/${site.siteId}`, site).pipe(
+      map((site: SiteDto) => new Site(site.siteId, site.name, site.icon, site.address))
+    );
+  }
+
+  delete(siteId: string): Observable<void> {
+    return this.http.delete<void>(`http://localhost:3000/api/sites/${siteId}`);
+  }
 }
--- src/app/features/private/dashboard/dashboard.ts
+++ src/app/features/private/dashboard/dashboard.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'hta-dashboard',
   imports: [ButtonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
