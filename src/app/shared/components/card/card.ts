7dc311e47cfec8378a5373c702d5e68100663f1e
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-04-05 20:01:55 -0300

    feat: add reusable Card component with content projection slots

---
+++ src/app/shared/components/card/card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hta-card',
+  imports: [],
+  templateUrl: './card.html',
+  styleUrl: './card.css',
+})
+export class Card {
+
+}
