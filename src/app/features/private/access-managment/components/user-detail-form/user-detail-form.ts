ec4bb726298354d770c0752caa2a23a928539362
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-08-19 19:24:09 -0300

    feat: implement UserDetailForm component for managing user access and permissions

--- src/app/features/private/access-managment/components/user-detail-form/user-detail-form.ts
+++ src/app/features/private/access-managment/components/user-detail-form/user-detail-form.ts
@@ -1,37 +1,55 @@
-import { Component, input, InputSignal } from '@angular/core';
-import { Avatar } from "primeng/avatar";
-import { Tag } from "primeng/tag";
-import { Dropdown } from "@shared/components/dropdown/dropdown";
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "primeng/tabs";
+import { Component, inject, input, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormsModule, Validators } from '@angular/forms';
 import { DrawerBody } from '@shared/components/drawer/interfaces/drawer-body';
 import { DrawerRef } from '@shared/components/drawer/interfaces/drawer-ref';
-import { Card } from "@shared/components/card/card";
-import { Button } from "@shared/components/button/button";
-import { UserDetailSummary } from "../user-detail-summary/user-detail-summary";
-import { InviteUserRoleAndScope } from "../invite-user-role-and-scope/invite-user-role-and-scope";
-import { InviteUserPermission } from "../invite-user-permission/invite-user-permission";
-
+import { FloatLabelInput } from "@shared/components/float-label-input/float-label-input";
+import { ReactiveFormsModule } from '@angular/forms';
+import { SpaceService } from '@core/index';
+import { AccessManagmentScope } from '../access-managment-scope/access-managment-scope';
+import { Checkbox } from 'primeng/checkbox';
+import { PermissionScope } from '@core/domain/models/permission-scope';
+import { UserManagment } from '../../models/user-managment';
+import { AccessManagmentService } from '../../services/access-managment.service';
 
 @Component({
   selector: 'hta-user-detail-form',
   imports: [
-    Avatar,
-    Tag,
-    Button,
-    Dropdown,
-    Tabs,
-    TabList,
-    Tab,
-    TabPanels,
-    TabPanel,
-    Button,
-    UserDetailSummary,
-    InviteUserRoleAndScope,
-    InviteUserPermission
+    ReactiveFormsModule, FloatLabelInput, AccessManagmentScope, Checkbox, FormsModule
   ],
   templateUrl: './user-detail-form.html',
   styleUrl: './user-detail-form.css',
 })
-export class UserDetailForm implements DrawerBody {
-  drawerRef = input.required<DrawerRef<unknown>>();
+export class UserDetailForm implements DrawerBody, OnInit {
+  drawerRef = input.required<DrawerRef>();
+  accessUser = input<UserManagment | null>();
+  isReadonly = input<boolean>(false);
+
+  #formBuilder = inject(FormBuilder);
+  #spacesService = inject(SpaceService);
+  #accessManagmentService = inject(AccessManagmentService);
+
+
+  topologyNodes = signal<PermissionScope[]>([]);
+  invitationForm = this.#formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required],
+    totalClient: [false]
+  });
+
+  ngOnInit(): void {
+    this.#spacesService.getTopology().subscribe((nodes: PermissionScope[]) => {
+      this.invitationForm.patchValue({
+        firstName: this.accessUser()?.firstName,
+        lastName: this.accessUser()?.lastName,
+        email: this.accessUser()?.email,
+        totalClient: false
+      });
+      this.topologyNodes.set(nodes);
+    });
+
+    this.#accessManagmentService.getAssigmentsByUser(this.accessUser()!.userId).subscribe((assigments) => {
+      console.log(assigments);
+    });
+  }
 }
