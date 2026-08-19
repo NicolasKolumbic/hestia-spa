202592f17bdac22eaad0e15e01d19a5247d525dd
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-08-19 18:46:14 -0300

    feat: add RolesDropdown component to provide selectable role options via service integration

--- src/app/core/components/roles-dropdown/roles-dropdown.ts
+++ src/app/core/components/roles-dropdown/roles-dropdown.ts
@@ -1,10 +1,11 @@
-import { Component, computed, inject, input, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, input, OnInit, output, signal } from '@angular/core';
 import { Dropdown } from "@shared/components/dropdown/dropdown";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HostControl } from '@shared/directives/host-control';
 import { ControlAccessor } from '@shared/abstractions/control-accessor';
 import { RolesService } from '@core/services/roles.service';
 import { DropdownOption } from '@shared/abstractions/dropdown/dropdown-option';
+import { RoleDto } from '@core/domain/dtos/role.dto';
 
 @Component({
   selector: 'hta-roles-dropdown',
@@ -20,21 +21,39 @@ export class RolesDropdown implements OnInit {
   #roleService = inject(RolesService);
 
   control = this.#hostControl.control;
-  options = signal<DropdownOption[]>([])
+  options = signal<DropdownOption[]>([]);
+
+  changes = output<DropdownOption>();
 
   ngOnInit(): void {
-    this.#roleService.getAllRoles().subscribe((roles) => {
-      this.options.set(roles.map((role) => ({
-        value: role.id,
-        label: role.name
-      })))
+    if (!this.options().length) {
+      this.#roleService.getAllRoles().subscribe((roles) => {
+        this.options.set(roles.map((role) => ({
+          value: role.id,
+          label: role.name
+        })))
 
-      if (this.setByName()) {
-        const role = this.options().find((role) => role.label === this.setByName());
-        if (role) {
-          this.#hostControl.setValue(role.value);
-        }
+        this.#init();
+      });
+    } else {
+      this.#init();
+    }
+  }
+
+  #init(): void {
+    if (this.setByName()) {
+      const role = this.options().find((role) => role.label === this.setByName());
+      if (role) {
+        this.#hostControl.setValue(role.value);
       }
-    });
+    }
+
+    this.#hostControl.control().valueChanges.subscribe((selectedRoleValue: string) => {
+      const role = this.options().find((role) => role.value === selectedRoleValue);
+      if (role) {
+        this.changes.emit(role);
+      }
+
+    })
   }
 }
