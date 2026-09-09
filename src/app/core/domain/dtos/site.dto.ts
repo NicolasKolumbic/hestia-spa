0d97a8a0f1f200cfd19b062ea02c4d06cb8546d0
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2026-09-09 20:29:24 -0300

    feat: add SiteDto domain model for site management

--- src/app/core/domain/dtos/site.dto.ts
+++ src/app/core/domain/dtos/site.dto.ts
@@ -1,6 +1,17 @@
+import { SiteStatus } from "@core/enums/site-status.enum";
+import { SiteType } from "@core/enums/site-type.enum";
+
 export interface SiteDto {
     siteId: string;
     name: string;
-    addressLine: string;
-    type: string;
+    type: SiteType;
+    status: SiteStatus;
+    address: string;
+    city: string;
+    countryCode: string;
+    locale: string;
+    province: string;
+    latitude: number;
+    longitude: number;
+    postalCode: string;
 }
